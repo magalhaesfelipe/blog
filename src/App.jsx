8968f8ca0202571425b670edd76739fff776fae4
magalhaesfelipe <felipemlopes0009@gmail.com>
2024-12-13 02:45:36 -0400

    Installed tailwind and added some styles to the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import "./index.css";
 
 function App() {
   return (
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,15 @@
 const Home = () => {
   return (
-    <div>
-      <div>
-        This is my home page
+    <div className="flex items-center justify-center h-screen bg-slate-800 text-white">
+      <div className="text-center">
+        <h1 className="text-4x1 font-bold mb-4 bg-red-600">Welcome to my Blog!</h1>
+        <div className="bg-yellow-500">
+        <p className="text-lg">This is the Home pageeeeeeee !</p>
+
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
