b7cb5abcb0dcf0f66598f558c099dbe4ab56a7b9
magalhaesfelipe <felipemlopes0009@gmail.com>
2024-12-18 14:56:14 -0400

    Adding a new font

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,16 +2,27 @@ const Home = () => {
   const x = 'something';
 
   return (
-    <div className='flex h-screen items-center justify-center bg-slate-800 text-white'>
-      {x}
-
+    <div className='flex h-screen items-center justify-center bg-gray-950 text-white font-mate'>
       <div className='text-center'>
-        <h1 className='text-4-1 mb-4 bg-red-600 font-bold'>
-          Welcome to my Blog!
-        </h1>
-        <div className='bg-yellow-500'>
-          <p className='font-serif text-lg font-semibold'>
-            This is the Home page !
+        <h1 className='text-4-1 mb-4 bg-red-600 font-bold'>Felipe Magalhaes</h1>
+        <div className='bg-yellow-500 max-w-96'>
+          <p className='text-lg font-semibold font-mate'>
+            Hey, I am Anthony Fu, a fanatical open sourceror. Working at
+            NuxtLabs Creator of Vitest Slidev VueUse UnoCSS Elk Type Challenges
+            Core team of Vue Nuxt Vite Maintaining Shiki Twoslash ESLint
+            Stylistic Dreaming up ideas and making them come true is where my
+            passion lies. You can find my full projects list here. I also do
+            some generative-art, compform, interactivity experiments on
+            100.antfu.me. I give talks and write blog posts about open source,
+            coding, tutorials, etc. Occasionally, I also do some coding live
+            streams on YouTube and 哔哩哔哩. Outside of programming, I enjoy
+            doing photography and traveling. Some of my photos can be found on
+            Instagram. Right now I live in Paris. If you are around, feel free
+            to reach me out, we could have some coffee or work together. Find me
+            on GitHub Bluesky Mastodon Twitter 中文推 日本語 Threads Discord
+            Server YouTube Instagram 哔哩哔哩 知乎 微博 Or mail me at hiantfu.me
+            If you enjoy my work and find them useful, consider sponsor me and
+            the ecosystem to help Open Source sustainable. Thank you!
           </p>
         </div>
       </div>
