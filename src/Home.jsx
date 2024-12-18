feac5c67abc258fa31f7af643db2e655e1411b75
magalhaesfelipe <felipemlopes0009@gmail.com>
2024-12-18 18:48:13 -0400

    Creating the Tech Stack section in the Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,134 @@
-const Home = () => {
-  const x = 'something';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+const Home = () => {
   return (
-    <div className='flex h-screen items-center justify-center bg-gray-950 text-white font-mate'>
-      <div className='text-center'>
-        <h1 className='text-4-1 mb-4 bg-red-600 font-bold'>Felipe Magalhaes</h1>
-        <div className='bg-yellow-500 max-w-96'>
-          <p className='text-lg font-semibold font-mate'>
-            Hey, I am Anthony Fu, a fanatical open sourceror. Working at
-            NuxtLabs Creator of Vitest Slidev VueUse UnoCSS Elk Type Challenges
-            Core team of Vue Nuxt Vite Maintaining Shiki Twoslash ESLint
-            Stylistic Dreaming up ideas and making them come true is where my
-            passion lies. You can find my full projects list here. I also do
-            some generative-art, compform, interactivity experiments on
-            100.antfu.me. I give talks and write blog posts about open source,
-            coding, tutorials, etc. Occasionally, I also do some coding live
-            streams on YouTube and 哔哩哔哩. Outside of programming, I enjoy
-            doing photography and traveling. Some of my photos can be found on
-            Instagram. Right now I live in Paris. If you are around, feel free
-            to reach me out, we could have some coffee or work together. Find me
-            on GitHub Bluesky Mastodon Twitter 中文推 日本語 Threads Discord
-            Server YouTube Instagram 哔哩哔哩 知乎 微博 Or mail me at hiantfu.me
-            If you enjoy my work and find them useful, consider sponsor me and
-            the ecosystem to help Open Source sustainable. Thank you!
-          </p>
+    <>
+      <header className='mt-10 flex justify-end'>
+        <a href='https://github.com/magalhaesfelipe' target='_blank'>
+          <FontAwesomeIcon
+            icon={faGithub}
+            className='mr-6 text-4xl text-white transition-colors'
+          />
+        </a>
+      </header>
+      <div className='mt-10 flex h-screen items-start justify-center font-mate text-white'>
+        <div className='text-left'>
+          <h1 className='mb-4 flex text-3xl'>Felipe Magalhaes</h1>
+          <div className='bg--500 max-w-fit'>
+            <p className='mb-32 font-mate text-lg'>
+              Hey, I'm Felipe. I create Full Stack applications. Check out my
+              projects.
+            </p>
+
+            <div>
+              <div className='mb-12 flex flex-col justify-center rounded-lg border-2 border-slate-700 p-5 align-middle'>
+                <div className='mb-10'>
+                  <div className='flex flex-col justify-center text-center'>
+                    <h3 className='mb-5 text-xl'>Projects</h3>
+                    <a
+                      href='https://mangazineprotocol.netlify.app'
+                      className='text-xl font-bold'
+                    >
+                      <FontAwesomeIcon
+                        icon={faGlobe}
+                        className='mr-2 text-2xl'
+                      />
+                      mangazineprotocol.netlify.app
+                    </a>
+                    <p className='text-slate-400'>
+                      A website to keep track of your reads
+                    </p>
+                  </div>
+                </div>
+                <div className='mb-5'>
+                  <div className='flex flex-col justify-center text-center align-top'>
+                    <a
+                      href='https://github.com/magalhaesfelipe/blog'
+                      className='text-xl font-bold'
+                    >
+                      <FontAwesomeIcon
+                        icon={faGlobe}
+                        className='mr-2 text-2xl'
+                      />
+                      This website
+                    </a>
+                    <p className='text-slate-400'>
+                      My website portfolio code on GitHub
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-col rounded-lg border-2 border-slate-700 p-5'>
+                <div className='flex flex-col text-center'>
+                  <h3 className='mb-5 text-xl'>Tech Stack</h3>
+                  <div className='flex justify-center align-top text-slate-400'>
+                    Full Stack Developer / Engineer
+                  </div>
+                  <div className='mt-5 grid grid-cols-6'>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Node.js
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Express.js
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      MongoDB
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Mongoose
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      SQL
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      React
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      TypeScript
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      JavaScript
+                    </p>
+                    <p className='mb-4 mr-4 text-nowrap rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Tailwind CSS
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      NPM
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Render
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Netflify
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Golang
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      HTML
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      CSS
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Postman
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Git
+                    </p>
+                    <p className='mb-4 mr-4 rounded-md bg-gray-700 p-1.5 hover:cursor-default hover:bg-gray-900'>
+                      Github
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
